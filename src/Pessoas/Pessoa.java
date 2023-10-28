00d5b9a3e3297273ac2d78f4087c5a7a81153eda
brunamagalhaesdesouza <bruna.magalhaesdesouza@gmail.com>
2023-10-27 21:03:59 -0300

    Adição das classes Funcionario, Cliente e Pessoa, ao projeto em questão.

---
+++ src/Pessoas/Pessoa.java
@@ -0,0 +1,50 @@
+package Pessoas;
+
+public class Pessoa {
+    private String nome;
+    private String dataNascimento;
+    private String endereco;
+    private String telsContato;
+
+    public void cadastrar() {
+
+    }
+
+    public int obterIdade() {
+        int idade;
+        // calculo de idade: ano atual - data de nascimento
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelsContato() {
+        return telsContato;
+    }
+
+    public void setTelsContato(String telsContato) {
+        this.telsContato = telsContato;
+    }
+}
